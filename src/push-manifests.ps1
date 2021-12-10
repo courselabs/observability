61ab41607adcf66c41ee0004bbf35e8269960d0e
Elton Stoneman <elton@sixeyed.com>
2021-12-10 20:16:58 +0000

    Manifest push

--- src/push-manifests.ps1
+++ src/push-manifests.ps1
@@ -1,17 +1,10 @@
-
-# TODO - replace with yq over compose.yml images
-$images=@(
-  'node-exporter:latest',
-  'prometheus:v2.28.1',
-  'grafana:8.0.5',
-  'jaeger:1.24'
-)
+$images=$(yq e '.services.[].image' compose.yml)
 
 foreach ($image in $images)
 {    
-    docker manifest create --amend "courselabs/$($image)" `
-      "courselabs/$($image)-linux-arm64" `
-      "courselabs/$($image)-linux-amd64"
+    docker manifest create --amend $image `
+      "$($image)-linux-arm64" `
+      "$($image)-linux-amd64"
     
-    docker manifest push "courselabs/$($image)"
+    docker manifest push $image
 }
